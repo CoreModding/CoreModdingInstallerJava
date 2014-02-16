1c1a7d54486d43464ecc8812a750d53468510bf4
Zeuslightning125 <zeusoflightning125@gmail.com>
2014-02-16 10:45:23 -0800

    Update WrapLayout.java

--- src/info/coremodding/coremoddinginstaller/frame/WrapLayout.java
+++ src/info/coremodding/coremoddinginstaller/frame/WrapLayout.java
@@ -9,9 +9,8 @@ import javax.swing.SwingUtilities;
  *  FlowLayout subclass that fully supports wrapping of components.
  */
 public class WrapLayout extends FlowLayout {
+	
 	private static final long serialVersionUID = 1L;
-	@SuppressWarnings("unused")
-	private Dimension preferredLayoutSize;
 
 	/**
 	* Constructs a new <code>WrapLayout</code> with a left
@@ -89,79 +88,50 @@ public class WrapLayout extends FlowLayout {
 	*/
 	private Dimension layoutSize(Container target, boolean preferred)
 	{
-	synchronized (target.getTreeLock())
-	{
-		//  Each row must fit with the width allocated to the containter.
-		//  When the container width = 0, the preferred width of the container
-		//  has not yet been calculated so lets ask for the maximum.
-
-		int targetWidth = target.getSize().width;
-
-		if (targetWidth == 0)
-			targetWidth = Integer.MAX_VALUE;
-
-		int hgap = getHgap();
-		int vgap = getVgap();
-		Insets insets = target.getInsets();
-		int horizontalInsetsAndGap = insets.left + insets.right + (hgap * 2);
-		int maxWidth = targetWidth - horizontalInsetsAndGap;
-
-		//  Fit components into the allowed width
-
-		Dimension dim = new Dimension(0, 0);
-		int rowWidth = 0;
-		int rowHeight = 0;
-
-		int nmembers = target.getComponentCount();
-
-		for (int i = 0; i < nmembers; i++)
+		synchronized (target.getTreeLock())
 		{
-			Component m = target.getComponent(i);
-
-			if (m.isVisible())
+			int targetWidth = target.getSize().width;
+			if (targetWidth == 0)
+				targetWidth = Integer.MAX_VALUE;
+			int hgap = getHgap();
+			int vgap = getVgap();
+			Insets insets = target.getInsets();
+			int horizontalInsetsAndGap = insets.left + insets.right + (hgap * 2);
+			int maxWidth = targetWidth - horizontalInsetsAndGap;
+			Dimension dim = new Dimension(0, 0);
+			int rowWidth = 0;
+			int rowHeight = 0;
+			int nmembers = target.getComponentCount();
+			for (int i = 0; i < nmembers; i++)
 			{
-				Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
-
-				//  Can't add the component to current row. Start a new row.
-
-				if (rowWidth + d.width > maxWidth)
+				Component m = target.getComponent(i);
+				if (m.isVisible())
 				{
-					addRow(dim, rowWidth, rowHeight);
-					rowWidth = 0;
-					rowHeight = 0;
+					Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
+					if (rowWidth + d.width > maxWidth)
+					{
+						addRow(dim, rowWidth, rowHeight);
+						rowWidth = 0;
+						rowHeight = 0;
+					}
+					if (rowWidth != 0)
+					{
+						rowWidth += hgap;
+					}
+					rowWidth += d.width;
+					rowHeight = Math.max(rowHeight, d.height);
 				}
-
-				//  Add a horizontal gap for all components after the first
-
-				if (rowWidth != 0)
-				{
-					rowWidth += hgap;
-				}
-
-				rowWidth += d.width;
-				rowHeight = Math.max(rowHeight, d.height);
 			}
+			addRow(dim, rowWidth, rowHeight);
+			dim.width += horizontalInsetsAndGap;
+			dim.height += insets.top + insets.bottom + vgap * 2;
+			Container scrollPane = SwingUtilities.getAncestorOfClass(JScrollPane.class, target);
+			if (scrollPane != null && target.isValid())
+			{
+				dim.width -= (hgap + 1);
+			}
+			return dim;
 		}
-
-		addRow(dim, rowWidth, rowHeight);
-
-		dim.width += horizontalInsetsAndGap;
-		dim.height += insets.top + insets.bottom + vgap * 2;
-
-		//	When using a scroll pane or the DecoratedLookAndFeel we need to
-		//  make sure the preferred size is less than the size of the
-		//  target containter so shrinking the container size works
-		//  correctly. Removing the horizontal gap is an easy way to do this.
-
-		Container scrollPane = SwingUtilities.getAncestorOfClass(JScrollPane.class, target);
-
-		if (scrollPane != null && target.isValid())
-		{
-			dim.width -= (hgap + 1);
-		}
-
-		return dim;
-	}
 	}
 
 	/*
