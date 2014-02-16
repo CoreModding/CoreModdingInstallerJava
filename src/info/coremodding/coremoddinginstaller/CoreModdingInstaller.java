03f2abe449af1558ef62c537fa09ced2423e3f8f
Zeuslightning125 <zeusoflightning125@gmail.com>
2014-02-16 10:42:41 -0800

    Update CoreModdingInstaller.java

--- src/info/coremodding/coremoddinginstaller/CoreModdingInstaller.java
+++ src/info/coremodding/coremoddinginstaller/CoreModdingInstaller.java
@@ -4,16 +4,14 @@ import info.coremodding.coremoddinginstaller.frame.CoreModdingInstallerFrame;
 
 public class CoreModdingInstaller {
 	
+	/**
+	 * The main entry point
+	 * 
+	 * @param args The command line arguments for the program
+	 */
 	public static void main(String[] args) {
-		//Mac application settings stuff
-		//public static Application application;
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "CORE Modding Installer");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.version", "v1.0");
-		//application = Application.getApplication();
-		//Image image = Toolkit.getDefaultToolkit().getImage("res/icon.png");
-		//application.setDockIconImage(image);
-		
 		new CoreModdingInstallerFrame();
 	}
-	
 }
