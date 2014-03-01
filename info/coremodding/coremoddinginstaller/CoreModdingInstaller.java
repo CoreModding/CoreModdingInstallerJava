68b019d048d888d21a0ee7005aa5e61260cf47e4
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-02-28 23:23:41 -0800

    Fixed warnings

--- info/coremodding/coremoddinginstaller/CoreModdingInstaller.java
+++ info/coremodding/coremoddinginstaller/CoreModdingInstaller.java
@@ -2,6 +2,9 @@ package info.coremodding.coremoddinginstaller;
 
 import info.coremodding.coremoddinginstaller.frame.CoreModdingInstallerFrame;
 
+/**
+ * The main class
+ */
 public class CoreModdingInstaller {
 	
 	/**
@@ -9,6 +12,7 @@ public class CoreModdingInstaller {
 	 * 
 	 * @param args The command line arguments for the program
 	 */
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "CORE Modding Installer");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.version", "v1.0");
--- info/coremodding/coremoddinginstaller/frame/CoreModdingInstallerFrame.java
+++ info/coremodding/coremoddinginstaller/frame/CoreModdingInstallerFrame.java
@@ -1,14 +1,15 @@
 package info.coremodding.coremoddinginstaller.frame;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import static org.apache.commons.io.FileUtils.copyURLToFile;
+
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
@@ -17,34 +18,43 @@ import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
+/**
+ * The JFrame for the installer
+ */
 public class CoreModdingInstallerFrame extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@SuppressWarnings("javadoc")
 	public static JFrame frmCoreModdingInstaller = new JFrame();
+	@SuppressWarnings("javadoc")
 	public static JPanel pnlCoreModdingInstaller = new JPanel();
 	
-	private String status;
-	private JCheckBox[] boxes;
+	String status;
+	JCheckBox[] boxes;
 	
 	private Scanner in;
 	private String url = "https://raw.github.com/CoreModding/CoreModdingInstaller/master/data";
-	private String data = "";
+	String data = "";
 	
-	private JPanel checkBoxes = new JPanel(new WrapLayout(WrapLayout.LEFT));
-	private JScrollPane checkedListBox1 = new JScrollPane(checkBoxes, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	private JPanel checkBoxes = new JPanel(new WrapLayout(FlowLayout.LEFT));
+	private JScrollPane checkedListBox1 = new JScrollPane(this.checkBoxes, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 	private JButton install = new JButton("Install/Update selected mods");
-	private JLabel statusLabel = new JLabel(status);
+	JLabel statusLabel = new JLabel(this.status);
 	
+	/**
+	 * The mainc onstructor
+	 */
 	public CoreModdingInstallerFrame() {
 
-	    checkedListBox1.setPreferredSize(new Dimension(234, 184));
-	    checkedListBox1.setSize(0, 1000);
-	    install.setPreferredSize(new Dimension(234, 21));
-	    pnlCoreModdingInstaller.add(checkedListBox1);
-	    pnlCoreModdingInstaller.add(install, BorderLayout.CENTER);
-	    pnlCoreModdingInstaller.add(statusLabel);
+	    this.checkedListBox1.setPreferredSize(new Dimension(234, 184));
+	    this.checkedListBox1.setSize(0, 1000);
+	    this.install.setPreferredSize(new Dimension(234, 21));
+	    pnlCoreModdingInstaller.add(this.checkedListBox1);
+	    pnlCoreModdingInstaller.add(this.install, BorderLayout.CENTER);
+	    pnlCoreModdingInstaller.add(this.statusLabel);
 	    frmCoreModdingInstaller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frmCoreModdingInstaller.getContentPane().add(pnlCoreModdingInstaller, BorderLayout.CENTER);
 	    frmCoreModdingInstaller.setTitle("CORE Modding Installer");
@@ -53,62 +63,50 @@ public class CoreModdingInstallerFrame extends JFrame {
 	    frmCoreModdingInstaller.setResizable(false);
 	    frmCoreModdingInstaller.setVisible(true);
 	    this.status = "Downloading data...";
-	    statusLabel.setText(status);
-	    try { in = new Scanner(new URL(url).openStream()); } catch(Exception e) { }
-	    if(in != null) while(in.hasNextLine()) { data += in.nextLine() + "\n" + ","; }
-	    if(!data.equals("")) { boxes = new JCheckBox[data.split(",").length]; }
-	    if(data != null) {
-	        for(int i = 0; i < data.split(",").length; i+=3) {
-	        	JCheckBox cb = new JCheckBox(data.split(",")[i].trim() + " - " + data.split(",")[i + 1] + new String(new char[100]).replace("\0", " ")); // Horrible yet the only way to make it wrap properly (that I know of)
-	        	boxes[i] = cb;
-	            checkBoxes.add(cb);
+	    this.statusLabel.setText(this.status);
+	    try { this.in = new Scanner(new URL(this.url).openStream()); } catch(Exception e) { }
+	    if(this.in != null) while(this.in.hasNextLine()) { this.data += this.in.nextLine() + "\n" + ","; }
+	    if(!this.data.equals("")) { this.boxes = new JCheckBox[this.data.split(",").length]; }
+	    if(this.data != null) {
+	        for(int i = 0; i < this.data.split(",").length; i+=3) {
+	        	JCheckBox cb = new JCheckBox(this.data.split(",")[i].trim() + " - " + this.data.split(",")[i + 1] + new String(new char[100]).replace("\0", " ")); // Horrible yet the only way to make it wrap properly (that I know of)
+	        	this.boxes[i] = cb;
+	            this.checkBoxes.add(cb);
 	        }
-	    } else { checkBoxes.add(new JLabel("No mod data available")); install.setEnabled(false); status = "ERROR"; statusLabel.setText(status); }
-	    install.addActionListener(new ActionListener() {
+	    } else { this.checkBoxes.add(new JLabel("No mod data available")); this.install.setEnabled(false); this.status = "ERROR"; this.statusLabel.setText(this.status); }
+	    this.install.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-		        status = "Installing Mods...";
-		        statusLabel.setText(status);
+		        CoreModdingInstallerFrame.this.status = "Installing Mods...";
+		        CoreModdingInstallerFrame.this.statusLabel.setText(CoreModdingInstallerFrame.this.status);
 		        String os = System.getProperty("os.name");
 		        String location = null;
 		        if(os.toLowerCase().startsWith("windows")) location = System.getenv("USERPROFILE") + "/appdata/roaming/.minecraft/mods/";
 		        if(os.toLowerCase().startsWith("mac")) location = System.getProperty("user.home") + "/Library/Application Support/minecraft/mods/";
 		        if(location == null) try { throw new Exception("OS not recognized!"); } catch(Exception e) { }
-		        for (int i = 0; i < boxes.length; i+=3) {
-		        	if(boxes[0].isSelected()) {
-		        		downloadFile(data.split(",")[i + 2], location + data.split(",")[i].trim() + data.split(",")[i + 1].trim() + data.split(",")[i + 2].substring(data.split(",")[2].length() - 5).trim());
+		        for (int i = 0; i < CoreModdingInstallerFrame.this.boxes.length; i+=3) {
+		        	if(CoreModdingInstallerFrame.this.boxes[0].isSelected()) {
+		        		downloadFile(CoreModdingInstallerFrame.this.data.split(",")[i + 2], location + CoreModdingInstallerFrame.this.data.split(",")[i].trim() + CoreModdingInstallerFrame.this.data.split(",")[i + 1].trim() + CoreModdingInstallerFrame.this.data.split(",")[i + 2].substring(CoreModdingInstallerFrame.this.data.split(",")[2].length() - 5).trim());
 		        	}
 		        	
 		        }
-		        status = "      Complete!      ";
-		        statusLabel.setText(status);
+		        CoreModdingInstallerFrame.this.status = "      Complete!      ";
+		        CoreModdingInstallerFrame.this.statusLabel.setText(CoreModdingInstallerFrame.this.status);
 			}
 	    });
 	    this.status = "Waiting for input...";
-	    statusLabel.setText(status);
+	    this.statusLabel.setText(this.status);
 	}
 	
-	public static void downloadFile(String URL, String location) {
-		URL url;
-        	URLConnection con;
-        	DataInputStream dis; 
-        	FileOutputStream fos; 
-        	byte[] fileData;  
-        	try {
-	            	url = new URL(URL);
-	            	con = url.openConnection();
-            		dis = new DataInputStream(con.getInputStream());
-            		fileData = new byte[con.getContentLength()]; 
-            		for (int q = 0; q < fileData.length; q++) { 
-                		fileData[q] = dis.readByte();
-            	}
-            	dis.close();
-            	fos = new FileOutputStream(new File(location));
-            	fos.write(fileData);
-            	fos.close();
-	        }
-        	catch(Exception m) {
-            	System.out.println(m);
-        	}
+	/**
+	 * @param url The url to download
+	 * @param location Where to download the file to
+	 */
+	public static void downloadFile(String url, String location) {
+		try {
+			copyURLToFile(new URL(url), new File(location));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
