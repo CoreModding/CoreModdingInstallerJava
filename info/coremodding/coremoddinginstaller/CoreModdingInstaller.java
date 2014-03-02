9be5b0e04c185e52932d6712f2abd98df98f1b53
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-01 17:14:34 -0800

    Fixed junk from old "formatting" and changed tabs to spaces.

--- info/coremodding/coremoddinginstaller/CoreModdingInstaller.java
+++ info/coremodding/coremoddinginstaller/CoreModdingInstaller.java
@@ -7,18 +7,17 @@ import info.coremodding.coremoddinginstaller.frame.CoreModdingInstallerFrame;
  */
 public class CoreModdingInstaller {
 
-	/**
-	 * The main entry point
-	 * 
-	 * @param args
-	 *            The command line arguments for the program
-	 */
-	@SuppressWarnings("unused")
-	public static void main(String[] args) {
-		System.setProperty("com.apple.mrj.application.apple.menu.about.name",
-				"CORE Modding Installer");
-		System.setProperty(
-				"com.apple.mrj.application.apple.menu.about.version", "v1.0");
-		new CoreModdingInstallerFrame();
-	}
+    /**
+     * The main entry point
+     *
+     * @param args The command line arguments for the program
+     */
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name",
+                "CORE Modding Installer");
+        System.setProperty(
+                "com.apple.mrj.application.apple.menu.about.version", "v1.0");
+        new CoreModdingInstallerFrame();
+    }
 }
--- info/coremodding/coremoddinginstaller/frame/CoreModdingInstallerFrame.java
+++ info/coremodding/coremoddinginstaller/frame/CoreModdingInstallerFrame.java
@@ -1,167 +1,157 @@
 package info.coremodding.coremoddinginstaller.frame;
 
-import static org.apache.commons.io.FileUtils.copyURLToFile;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.URL;
 import java.util.Scanner;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
+import static org.apache.commons.io.FileUtils.copyURLToFile;
 
 /**
  * The JFrame for the installer
  */
 public class CoreModdingInstallerFrame extends JFrame {
 
-	@SuppressWarnings("javadoc")
-	public static JFrame frmCoreModdingInstaller = new JFrame();
+    @SuppressWarnings("javadoc")
+    public static JFrame frmCoreModdingInstaller = new JFrame();
 
-	@SuppressWarnings("javadoc")
-	public static JPanel pnlCoreModdingInstaller = new JPanel();
-	private static final long serialVersionUID = 1L;
+    @SuppressWarnings("javadoc")
+    public static JPanel pnlCoreModdingInstaller = new JPanel();
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @param url
-	 *            The url to download
-	 * @param location
-	 *            Where to download the file to
-	 */
-	public static void downloadFile(String url, String location) {
-		try {
-			copyURLToFile(new URL(url), new File(location));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	JCheckBox[] boxes;
+    /**
+     * @param url      The url to download
+     * @param location Where to download the file to
+     */
+    public static void downloadFile(String url, String location) {
+        try {
+            copyURLToFile(new URL(url), new File(location));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private JPanel checkBoxes = new JPanel(new WrapLayout(FlowLayout.LEFT));
-	private JScrollPane checkedListBox1 = new JScrollPane(this.checkBoxes,
-			ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
-			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-	String data = "";
+    JCheckBox[] boxes;
 
-	private Scanner in;
-	private JButton install = new JButton("Install/Update selected mods");
-	String status;
-	JLabel statusLabel = new JLabel(this.status);
+    private JPanel checkBoxes = new JPanel(new WrapLayout(FlowLayout.LEFT));
+    private JScrollPane checkedListBox1 = new JScrollPane(this.checkBoxes,
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    String data = "";
 
-	private String url = "https://raw.github.com/CoreModding/CoreModdingInstaller/master/data";
+    private Scanner in;
+    private JButton install = new JButton("Install/Update selected mods");
+    String status;
+    JLabel statusLabel = new JLabel(this.status);
 
-	/**
-	 * The mainc onstructor
-	 */
-	public CoreModdingInstallerFrame() {
+    private String url = "https://raw.github.com/CoreModding/CoreModdingInstaller/master/data";
 
-		this.checkedListBox1.setPreferredSize(new Dimension(234, 184));
-		this.checkedListBox1.setSize(0, 1000);
-		this.install.setPreferredSize(new Dimension(234, 21));
-		pnlCoreModdingInstaller.add(this.checkedListBox1);
-		pnlCoreModdingInstaller.add(this.install, BorderLayout.CENTER);
-		pnlCoreModdingInstaller.add(this.statusLabel);
-		frmCoreModdingInstaller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCoreModdingInstaller.getContentPane().add(pnlCoreModdingInstaller,
-				BorderLayout.CENTER);
-		frmCoreModdingInstaller.setTitle("CORE Modding Installer");
-		frmCoreModdingInstaller.setSize(new Dimension(258, 275));
-		frmCoreModdingInstaller.setLocationRelativeTo(null);
-		frmCoreModdingInstaller.setResizable(false);
-		frmCoreModdingInstaller.setVisible(true);
-		this.status = "Downloading data...";
-		this.statusLabel.setText(this.status);
-		try {
-			this.in = new Scanner(new URL(this.url).openStream());
-		} catch (Exception e) {
-		}
-		if (this.in != null)
-			while (this.in.hasNextLine()) {
-				this.data += this.in.nextLine() + "\n" + ",";
-			}
-		if (!this.data.equals("")) {
-			this.boxes = new JCheckBox[this.data.split(",").length];
-		}
-		if (this.data != null) {
-			for (int i = 0; i < this.data.split(",").length; i += 3) {
-				JCheckBox cb = new JCheckBox(this.data.split(",")[i].trim()
-						+ " - " + this.data.split(",")[i + 1]
-						+ new String(new char[100]).replace("\0", " ")); // Horrible
-																			// yet
-																			// the
-																			// only
-																			// way
-																			// to
-																			// make
-																			// it
-																			// wrap
-																			// properly
-																			// (that
-																			// I
-																			// know
-																			// of)
-				this.boxes[i] = cb;
-				this.checkBoxes.add(cb);
-			}
-		} else {
-			this.checkBoxes.add(new JLabel("No mod data available"));
-			this.install.setEnabled(false);
-			this.status = "ERROR";
-			this.statusLabel.setText(this.status);
-		}
-		this.install.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				CoreModdingInstallerFrame.this.status = "Installing Mods...";
-				CoreModdingInstallerFrame.this.statusLabel
-						.setText(CoreModdingInstallerFrame.this.status);
-				String os = System.getProperty("os.name");
-				String location = null;
-				if (os.toLowerCase().startsWith("windows"))
-					location = System.getenv("USERPROFILE")
-							+ "/appdata/roaming/.minecraft/mods/";
-				if (os.toLowerCase().startsWith("mac"))
-					location = System.getProperty("user.home")
-							+ "/Library/Application Support/minecraft/mods/";
-				if (location == null)
-					try {
-						throw new Exception("OS not recognized!");
-					} catch (Exception e) {
-					}
-				for (int i = 0; i < CoreModdingInstallerFrame.this.boxes.length; i += 3) {
-					if (CoreModdingInstallerFrame.this.boxes[0].isSelected()) {
-						downloadFile(
-								CoreModdingInstallerFrame.this.data.split(",")[i + 2],
-								location
-										+ CoreModdingInstallerFrame.this.data
-												.split(",")[i].trim()
-										+ CoreModdingInstallerFrame.this.data
-												.split(",")[i + 1].trim()
-										+ CoreModdingInstallerFrame.this.data
-												.split(",")[i + 2]
-												.substring(
-														CoreModdingInstallerFrame.this.data
-																.split(",")[2]
-																.length() - 5)
-												.trim());
-					}
+    /**
+     * The mainc onstructor
+     */
+    public CoreModdingInstallerFrame() {
 
-				}
-				CoreModdingInstallerFrame.this.status = "      Complete!      ";
-				CoreModdingInstallerFrame.this.statusLabel
-						.setText(CoreModdingInstallerFrame.this.status);
-			}
-		});
-		this.status = "Waiting for input...";
-		this.statusLabel.setText(this.status);
-	}
+        this.checkedListBox1.setPreferredSize(new Dimension(234, 184));
+        this.checkedListBox1.setSize(0, 1000);
+        this.install.setPreferredSize(new Dimension(234, 21));
+        pnlCoreModdingInstaller.add(this.checkedListBox1);
+        pnlCoreModdingInstaller.add(this.install, BorderLayout.CENTER);
+        pnlCoreModdingInstaller.add(this.statusLabel);
+        frmCoreModdingInstaller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmCoreModdingInstaller.getContentPane().add(pnlCoreModdingInstaller,
+                BorderLayout.CENTER);
+        frmCoreModdingInstaller.setTitle("CORE Modding Installer");
+        frmCoreModdingInstaller.setSize(new Dimension(258, 275));
+        frmCoreModdingInstaller.setLocationRelativeTo(null);
+        frmCoreModdingInstaller.setResizable(false);
+        frmCoreModdingInstaller.setVisible(true);
+        this.status = "Downloading data...";
+        this.statusLabel.setText(this.status);
+        try {
+            this.in = new Scanner(new URL(this.url).openStream());
+        } catch (Exception e) {
+        }
+        if (this.in != null)
+            while (this.in.hasNextLine()) {
+                this.data += this.in.nextLine() + "\n" + ",";
+            }
+        if (!this.data.equals("")) {
+            this.boxes = new JCheckBox[this.data.split(",").length];
+        }
+        if (this.data != null) {
+            for (int i = 0; i < this.data.split(",").length; i += 3) {
+                JCheckBox cb = new JCheckBox(this.data.split(",")[i].trim()
+                        + " - " + this.data.split(",")[i + 1]
+                        + new String(new char[100]).replace("\0", " ")); // Horrible
+                // yet
+                // the
+                // only
+                // way
+                // to
+                // make
+                // it
+                // wrap
+                // properly
+                // (that
+                // I
+                // know
+                // of)
+                this.boxes[i] = cb;
+                this.checkBoxes.add(cb);
+            }
+        } else {
+            this.checkBoxes.add(new JLabel("No mod data available"));
+            this.install.setEnabled(false);
+            this.status = "ERROR";
+            this.statusLabel.setText(this.status);
+        }
+        this.install.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                CoreModdingInstallerFrame.this.status = "Installing Mods...";
+                CoreModdingInstallerFrame.this.statusLabel
+                        .setText(CoreModdingInstallerFrame.this.status);
+                String os = System.getProperty("os.name");
+                String location = null;
+                if (os.toLowerCase().startsWith("windows"))
+                    location = System.getenv("USERPROFILE")
+                            + "/appdata/roaming/.minecraft/mods/";
+                if (os.toLowerCase().startsWith("mac"))
+                    location = System.getProperty("user.home")
+                            + "/Library/Application Support/minecraft/mods/";
+                if (location == null)
+                    try {
+                        throw new Exception("OS not recognized!");
+                    } catch (Exception e) {
+                    }
+                for (int i = 0; i < CoreModdingInstallerFrame.this.boxes.length; i += 3) {
+                    if (CoreModdingInstallerFrame.this.boxes[0].isSelected()) {
+                        downloadFile(
+                                CoreModdingInstallerFrame.this.data.split(",")[i + 2],
+                                location
+                                        + CoreModdingInstallerFrame.this.data
+                                        .split(",")[i].trim()
+                                        + CoreModdingInstallerFrame.this.data
+                                        .split(",")[i + 1].trim()
+                                        + CoreModdingInstallerFrame.this.data
+                                        .split(",")[i + 2]
+                                        .substring(
+                                                CoreModdingInstallerFrame.this.data
+                                                        .split(",")[2]
+                                                        .length() - 5)
+                                        .trim());
+                    }
+
+                }
+                CoreModdingInstallerFrame.this.status = "      Complete!      ";
+                CoreModdingInstallerFrame.this.statusLabel
+                        .setText(CoreModdingInstallerFrame.this.status);
+            }
+        });
+        this.status = "Waiting for input...";
+        this.statusLabel.setText(this.status);
+    }
 }
